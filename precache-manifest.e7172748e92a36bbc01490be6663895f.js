595b1b210309fa14cd1674305045892c81deac95
Nguyen Thanh Dat <bacbia3696@gmail.com>
2020-07-11 11:25:22 +0700

    Updates

---
+++ precache-manifest.e7172748e92a36bbc01490be6663895f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c24eabd5c8f04acad74fbe56cc3943",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "753880965fc645a78481",
+    "url": "/crwn-clothing/static/css/main.945488d9.chunk.css"
+  },
+  {
+    "revision": "54742a37892833cb7757",
+    "url": "/crwn-clothing/static/js/2.0ec3e34d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crwn-clothing/static/js/2.0ec3e34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753880965fc645a78481",
+    "url": "/crwn-clothing/static/js/main.5a6bf6b1.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  }
+]);
